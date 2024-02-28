79f52e52e76584bffd87f193d3dafde37e77e4e9
mantelli96 <mantelli96@libero.it>
2024-02-28 02:19:31 +0100

    First commit

--- Memory.Library/Board.cs
+++ Memory.Library/Board.cs
@@ -8,25 +8,30 @@ namespace Memory.Library;
 public class Board
 {
     private readonly Difficulty _difficulty;
-    private int BoardDimention => 
+    private int BoardDimention =>
         Constants.GetBoardDimention(_difficulty);
-    private int BoardSize => 
+    private int BoardSize =>
         Constants.GetBoardSize(_difficulty);
-    private ImmutableList<int> MemoryValues => 
-        Constants.GetMemoryValues(BoardSize).ToList().Shuffle();
-    private ImmutableList<Cell> GameBoard {  get; }
+    private ImmutableList<int> MemoryValues { get; }
+    private ImmutableList<Cell> GameBoard { get; }
+    public IEnumerable<Cell> GetGameBoard => GameBoard;
+    public Cell this[int index]
+    {
+        get => index < GameBoard.Count ?
+            GameBoard[index] : throw new ArgumentException("Index out fo board range");
+    }
     private Board(Difficulty difficulty)
     {
         _difficulty = difficulty;
+        MemoryValues = Constants.GetMemoryValues(BoardSize).Shuffle();
         GameBoard = [.. GenerateBoard()];
     }
     public static Board Initialize(Difficulty difficulty) => new(difficulty);
-    
+
     private IEnumerable<Cell> GenerateBoard() =>
         Enumerable.Range(0, BoardSize)
         .Select(i => new Cell(
             i / BoardDimention,
             i % BoardDimention,
             MemoryValues[i]));
-
 }
--- Memory.Library/Cell.cs
+++ Memory.Library/Cell.cs
@@ -1,18 +1,17 @@
 ﻿namespace Memory.Library;
 
-public class Cell(int row, int col, int value) : IEquatable<Cell>
+public readonly struct Cell(int row, int col, int value) : IEquatable<Cell>
 {
     public int X { get; } = row;
     public int Y { get; } = col;    
-    public int Value { get; } = value;
+    public int MemoryValue { get; } = value;
 
     public override bool Equals(object? obj) =>
         obj is Cell cell && Equals(cell);
-    public bool Equals(Cell? other) =>
-       this is null ? other is null :
-       Value == other?.Value;
+    public bool Equals(Cell other) =>
+       MemoryValue == other.MemoryValue;
     public static bool operator ==(Cell? a, Cell? b) => a is null ? b is null : a.Equals(b);
     public static bool operator !=(Cell? a, Cell? b) => !(a == b);
-    public override int GetHashCode() => Value.GetHashCode();
-   
+    public override int GetHashCode() => MemoryValue.GetHashCode();
+
 }
