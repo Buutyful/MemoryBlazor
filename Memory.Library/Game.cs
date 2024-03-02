173fc85bd824e9018162a0d38f1838abeb4d0aa5
mantelli96 <mantelli96@libero.it>
2024-03-02 18:02:33 +0100

    Revert "basic funtionalities done"
    This reverts commit 3894ffb9011b11739489a9150b2a65c5768aa42b.

--- Memory.Library/Game.cs
+++ Memory.Library/Game.cs
@@ -4,20 +4,20 @@ public class Game
 {
     private readonly Timer _timer;
     public Board Board { get; }
-    public GameState State { get; private set; } = GameState.Going;
+    public GameState State { get; private set; }
     public int CountDown { get; private set; }
     public int MovesCount { get; private set; }
     public List<Card> CorrectCards { get; private set; } = [];
     public Result? Result { get; private set; }
     public event EventHandler<int> TimerElapsed;
     private Game(Difficulty difficulty)
-    {
+    {       
         Board = Board.Initialize(difficulty);
         CountDown = 120 * (int)difficulty;
         _timer = new Timer(OnTimerElapsed, null, 1000, 1000);
     }
     public static Game Initialize(Difficulty difficulty)
-    {
+    {        
         return new Game(difficulty);
     }
     public bool MakeMove(Card card1, Card card2)
@@ -38,8 +38,8 @@ public class Game
         if (CountDown == 0)
         {
             State = GameState.Over;
-            Result = Result.Lost(EndReason.TimeOut, this);
-
+            Result = Result.Lost(EndReason.TimeOut);
+            UnsubscribeFromTimerElapsed();
         }
 
         TimerElapsed.Invoke(this, CountDown);
@@ -54,29 +54,19 @@ public class Game
     private void CheckGameOver()
     {
         if (Board.BoardSize == CorrectCards.Count)
-        {
-            UnsubscribeFromTimerElapsed();
             State = GameState.Over;
-        }
-        Result = Result.Win(this);
+        Result = Result.Win();
     }
 
 }
 public record Result
 {
     public EndReason EndReason { get; }
-    public Game Game { get; }
-    private Result(EndReason reason, Game game) =>
-        (EndReason, Game) = (reason, game);
-    public static Result Win(Game game)
-        => new(EndReason.Win, game);
-    public static Result Lost(EndReason reason, Game game)
-        => new(reason, game);
-    public string DisplayResult() =>
-        $"{EndReason},\n" +
-        $"Total moves: {Game.MovesCount},\n" +
-        $"Time Left: {Game.CountDown},\n" +
-        $"Total Cards: {Game.CorrectCards.Count}";
+    private Result(EndReason reason) => EndReason = reason;
+    public static Result Win()
+        => new(EndReason.Win);
+    public static Result Lost(EndReason reason)
+        => new(reason);
 }
 public enum EndReason
 {
